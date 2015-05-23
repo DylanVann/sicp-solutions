7f51f869116cd4121c6156b24ca51d82b35f5216
Dylan Vann <dylan.vann@gmail.com>
2015-05-22 23:20:26 -0400

    Add sicp-ex-1.8.scm.

---
+++ sicp-ex-1.8.scm
@@ -0,0 +1,19 @@
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+       guess
+       (cube-root-iter (improve guess x) x)))
+
+; Should return ~2.
+(cube-root-iter 1.0 8)
