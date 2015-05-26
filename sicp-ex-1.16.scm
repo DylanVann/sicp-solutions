64ba005e0d8fa6f153fe60547e55f49c417aaaf6
Dylan Vann <dylan.vann@gmail.com>
2015-05-25 21:04:05 -0400

    - Add square function. - Add fast-exp function for comparison.

--- sicp-ex-1.16.scm
+++ sicp-ex-1.16.scm
@@ -1,11 +1,24 @@
 ; This only works for integers.
+(define (square x)
+  (* x x))
+
+(define (fast-exp b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-exp b (/ n 2))))
+        (else (* b (fast-exp b (- n 1))))))
+;Tests
+(= (fast-exp 2 2) 4)
+(= (fast-exp 2 3) 8)
+(= (fast-exp 3 2) 9)
+(= (fast-exp 4 2) 16)
+
 (define (faster-exp b n)
   (define (faster-exp-iter b n a)
     (cond ((= n 0) a)
-          ((even? n) (faster-exp-iter (* b b) (/ n 2) a))
+          ((even? n) (faster-exp-iter (square b) (/ n 2) a))
           (else (faster-exp-iter b (- n 1) (* b a)))))
   (faster-exp-iter b n 1))
-
+;Tests
 (= (faster-exp 2 2) 4)
 (= (faster-exp 2 3) 8)
 (= (faster-exp 3 2) 9)
